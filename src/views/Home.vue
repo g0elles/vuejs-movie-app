2d07dfa6f38ad06385a57c2f5e15ad638d3b7325
Gabri Elles <ellesgabry5@gmail.com>
2021-11-03 23:53:31 -0500

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <navbar />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '../components/Navbar'
+export default {
+  name: 'Home',
+  components: {
+    Navbar,
+  }
+}
+</script>
