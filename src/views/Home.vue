71e5a2dcad40277225c90d4b502fa2c4bf9f48ad
Gabri Elles <ellesgabry5@gmail.com>
2021-11-04 00:03:19 -0500

    second commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="home">
     <navbar />
+    <div id="main-section">
+      <sidebar />
+      <movie-list />
+
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+import MovieList from '../components/MovieList.vue'
+
 export default {
   name: 'Home',
   components: {
     Navbar,
+    Sidebar,
+    MovieList
   }
 }
 </script>
